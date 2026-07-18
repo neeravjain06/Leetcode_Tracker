da1db0942e6b047a0e45f0aea3fa4f12d98181ef
Neerav Jain <neeravjain1006@gmail.com>
2026-07-18 14:34:41 +0530

    File updated at 18/07/2026, 14:34:41

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.py
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -1,21 +1,21 @@
-# Last updated: 15/07/2026, 18:29:08
+# Last updated: 18/07/2026, 14:34:41
 1class Solution(object):
 2    def maxArea(self, height):
-3        left = 0
-4        right = len(height) - 1
+3        left=0
+4        right=len(height)-1
 5
-6        maximum = 0
+6        max=0
 7
-8        while left < right:
-9
-10            width = right - left
-11            curr = width * min(height[left], height[right])
-12
-13            maximum = max(maximum, curr)
-14
-15            if height[left] < height[right]:
-16                left += 1
-17            else:
-18                right -= 1
+8        while left<right:
+9            curr=(right-left)*min(height[left],height[right])
+10
+11            if max<curr:
+12                max=curr
+13            
+14            if height[left]<height[right]:
+15                left+=1
+16            else:
+17                right-=1
+18        return max
 19
-20        return maximum
+20
