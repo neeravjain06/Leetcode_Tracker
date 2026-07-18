c4ba9de7d6ba44f556f1cd49d2a30a89e8903967
Neerav Jain <neeravjain1006@gmail.com>
2026-07-18 15:15:47 +0530

    File updated at 18/07/2026, 15:15:47

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,33 +1,23 @@
-# Last updated: 16/07/2026, 23:03:25
+# Last updated: 18/07/2026, 15:15:47
 1class Solution(object):
 2    def trap(self, height):
-3        if not height:
-4            return 0
+3        left=0
+4        right=len(height)-1
 5
-6        left = 0
-7        right = len(height) - 1
+6        leftMax=height[left]
+7        rightMax=height[right]
 8
-9        leftMax = height[left]
-10        rightMax = height[right]
-11
-12        water = 0
-13
-14        while left < right:
-15
-16            if leftMax < rightMax:
-17
-18                left += 1
-19
-20                leftMax = max(leftMax, height[left])
-21
-22                water += leftMax - height[left]
-23
-24            else:
-25
-26                right -= 1
-27
-28                rightMax = max(rightMax, height[right])
-29
-30                water += rightMax - height[right]
-31
-32        return water
+9        water=0
+10
+11        while left<right:
+12
+13            if leftMax<rightMax:
+14                left+=1
+15                leftMax=max(leftMax,height[left])
+16                water+=leftMax-height[left]
+17            else:
+18                right-=1
+19                rightMax=max(rightMax,height[right])
+20                water+=rightMax-height[right]
+21        return water 
+22
