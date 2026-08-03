2ce18a53ae2ab10964839855b372595011e975f1
Neerav Jain <neeravjain1006@gmail.com>
2026-08-03 09:02:54 +0530

    File updated at 03/08/2026, 09:02:53

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,23 +1,15 @@
-# Last updated: 18/07/2026, 15:15:47
+# Last updated: 03/08/2026, 09:02:53
 1class Solution(object):
-2    def trap(self, height):
-3        left=0
-4        right=len(height)-1
-5
-6        leftMax=height[left]
-7        rightMax=height[right]
-8
-9        water=0
-10
-11        while left<right:
-12
-13            if leftMax<rightMax:
-14                left+=1
-15                leftMax=max(leftMax,height[left])
-16                water+=leftMax-height[left]
-17            else:
-18                right-=1
-19                rightMax=max(rightMax,height[right])
-20                water+=rightMax-height[right]
-21        return water 
-22
+2    def isValid(self, s):
+3        stack = []
+4        pairs = {')': '(', ']': '[', '}': '{'}
+5        
+6        for ch in s:
+7            if ch in "({[":
+8                stack.append(ch)
+9            else:
+10                if not stack or stack[-1] != pairs[ch]:
+11                    return False
+12                stack.pop()
+13        
+14        return len(stack) == 0
