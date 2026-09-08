24df73fd0c289f1b976319da485a52c7a2eb88ab
Neerav Jain <neeravjain1006@gmail.com>
2026-09-08 09:17:28 +0530

    File updated at 08/09/2026, 09:17:27

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,11 +1,10 @@
-# Last updated: 06/07/2026, 21:55:37
-class Solution(object):
-    def containsDuplicate(self, nums):
-        seen=set()   
-        for i in nums:
-            if i in seen:
-                return True
-            seen.add(i)
-        return False
-
-        
+# Last updated: 08/09/2026, 09:17:27
+1class Solution(object):
+2    def containsDuplicate(self, nums):
+3        seen=set()
+4        for i in nums:
+5            if i in seen:
+6                return True
+7            seen.add(i)
+8        return False
+9        
