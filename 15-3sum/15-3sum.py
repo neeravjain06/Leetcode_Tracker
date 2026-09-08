386443202526d6e9f2e7ef23febe1c669626ad99
Neerav Jain <neeravjain1006@gmail.com>
2026-09-08 09:18:27 +0530

    Create 15-3sum

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,40 @@
+# Last updated: 08/09/2026, 09:18:26
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        ans = []
+
+        for i in range(len(nums)):
+
+            # Skip duplicate first numbers
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+
+                elif total > 0:
+                    right -= 1
+
+                else:
+                    ans.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    # Skip duplicate left values
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+                    # Skip duplicate right values
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return ans
